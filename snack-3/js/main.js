9ac71401cffd0a432ab00db2d9deb1268e51bbf9
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-08 17:32:15 +0100

    snack 3 completo

--- snack-3/js/main.js
+++ snack-3/js/main.js
@@ -1,24 +1,46 @@
 /*
 Snack 3
-Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
-Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa 
+con le seguenti proprietà: nome e peso. 
+Stampare a schermo la bici con peso minore utilizzando destructuring e 
+template literal.
 */
 
 // creo array bici
 
-let bici = [
+const bici = [
 
     {
-        nome: Scott,
+        nome: 'Scott',
         peso: 8
     },
     {
-        nome: Cannondale,
+        nome: 'Cannondal',
         peso: 10
     },
     {
-        nome: Cervelo,
+        nome: 'Cervelo',
         peso: 6
+    },
+    {
+        nome: 'Einhorn',
+        peso: 5
     }
 ];
 
+
+let limite = 20;
+let pesoMinore;
+
+for(let i = 0; i < bici.length; i++){
+    
+    let {nome, peso} = bici[i];
+    
+    if(peso < limite){
+        
+        limite=peso;
+        pesoMinore = nome;
+    }
+}
+
+console.log(`la bici più leggera è ${pesoMinore} e pesa ${limite} kg`);
