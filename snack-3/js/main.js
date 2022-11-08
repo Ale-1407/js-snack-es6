d2ac797c9cd2a1f0e7e4bf4ab54bc11ce0405669
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-08 15:16:47 +0100

    array di oggetti

--- snack-3/js/main.js
+++ snack-3/js/main.js
@@ -3,3 +3,22 @@ Snack 3
 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. 
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
 */
+
+// creo array bici
+
+let bici = [
+
+    {
+        nome: Scott,
+        peso: 8
+    },
+    {
+        nome: Cannondale,
+        peso: 10
+    },
+    {
+        nome: Cervelo,
+        peso: 6
+    }
+];
+
